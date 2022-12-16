69ac15977e42e0c527992a91a3541ed82f3c5b77
XenoProxy <ksenomorph6@gmail.com>
2022-12-16 18:16:55 +0300

    [+] message.php [*] Feedback form

---
+++ message.php
@@ -0,0 +1,7 @@
+<?php require_once './header.php'; ?>
+
+<div>
+    <h1>Your message has been sent successfully!<br>You will be redirected to the Contacts page automatically.</h1>
+</div>
+
+<?php require_once './footer.php';
--- page.php
+++ page.php
@@ -4,10 +4,17 @@ $page = $pagesData[$_GET['pid']]; ?>
 <h1><?php echo $page['name']; ?></h1>
 <div class="content"><?php echo $page['content']; ?></div>
 
-<?php if($page == 'contact'):
+<?php if($page['name'] == 'Contact'): ?>
 
-// форма
+<h2>Feedback</h2>
+<form action = "message.php" method="post">
+ <p>Name: <input type="text" name="name" /></p>
+ <p>Phone: <input type="text" name="phone" /></p>
+ <p>Email: <input type="text" name="email" /></p>
+ <p>Message: <input type="text" name="message" /></p>
+ <p><input type="submit" /></p>
+</form>
 
-endif; ?>
+<?php endif; ?>
 
 <?php endif; ?>
